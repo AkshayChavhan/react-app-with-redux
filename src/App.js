2e5d667eacb8ac378ac77e84ccf7f540ce41f5e5
Rohit Londhe <akshay@rtledgers.com>
2023-08-09 10:32:02 +0530

    Redux added in index and app file only

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+
+const App = () => {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const handleClickAdd = () => dispatch({ type: "ADD" });
+  const handleClickRemove = () => dispatch({ type: "REMOVE" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <h1>{count}</h1>
+    <button onClick={handleClickAdd}>Add</button>
+    <button onClick={handleClickRemove}>Remove</button>
+    </>
+  )
+};
 
 export default App;
